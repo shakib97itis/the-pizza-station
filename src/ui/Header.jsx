c8cb16f11ac28c72a9a12ace6810e9230bc9da7e
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-10-12 16:48:54 +0600

    Refactor Header component to update background color for improved styling consistency

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
 const Header = () => {
   return (
-    <div className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <div className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
