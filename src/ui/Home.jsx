ec7930721df3e73134369ff7cdc0056fa02371d4
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-10-12 12:03:02 +0600

    Refactor Home component for improved layout and styling

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,8 +2,8 @@ import CreateUser from "../features/user/CreateUser";
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-center text-xl font-semibold mb-4">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-4 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
