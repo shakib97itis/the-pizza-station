ed214baa6396af78eda225ea14d3b209edb5338e
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-10-12 16:48:40 +0600

    Refactor NotFound component to use LinkButton for improved consistency and styling

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,14 +1,14 @@
-import { useRouteError, useNavigate } from "react-router";
+import { useRouteError } from "react-router";
+import LinkButton from "./LinkButton";
 
 function NotFound() {
-  const navigate = useNavigate();
   const error = useRouteError();
-  
+
   return (
     <div>
       <h1>Something went wrong 😢</h1>
       <p>{error.message || error.statusText}</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+      <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
   );
 }
