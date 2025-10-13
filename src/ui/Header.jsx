40f5e96b9c764f688e57a9bf457ed07f8141a946
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-10-13 20:27:35 +0600

    Update Header component title and styling for improved branding consistency

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,8 +5,8 @@ import Username from "../features/user/Username";
 const Header = () => {
   return (
     <div className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className="tracking-widest">
-        Fast React Pizza Co.
+      <Link to="/" className="tracking-widest font-semibold">
+        The Pizza Station
       </Link>
       <SearchOrder />
       <Username />
