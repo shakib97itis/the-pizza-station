51519ebb6ac65e779159c2ec233f8a9128a57949
Shakib <41319856+shakib97itis@users.noreply.github.com>
2025-10-14 22:45:33 +0600

    Refactor CartOverview to use selectors for total quantity and price, removing console log and hardcoded values

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,14 +1,18 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
 function CartOverview() {
-  const x = useSelector((state) => state.cart.cart);
-  console.log(x);
+  const cartTotalQuantity = useSelector(getTotalCartQuantity);
+  const cartTotalPrice = useSelector(getTotalCartPrice);
+
+  if (!cartTotalQuantity) return null;
+  
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 text-sm text-stone-200 uppercase sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{cartTotalQuantity} pizzas</span>
+        <span>${cartTotalPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
