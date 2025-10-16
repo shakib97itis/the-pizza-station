cacfc173f76628b9a35375d5f5ea4a1db4c07387
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-10-16 21:14:52 +0600

    Button updated

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router";
 
-const Button = ({ children, disabled, type = "primary", to, onClick }) => {
+const Button = ({
+  children,
+  disabled,
+  type = "primary",
+  to,
+  onClick,
+}) => {
   const baseStyles =
     "inline-block text-sm cursor-pointer rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 ease-in-out hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-0 disabled:cursor-not-allowed";
 
