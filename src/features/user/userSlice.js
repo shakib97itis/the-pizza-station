a35f77ce4d13313ec6ea7cbbed77aecbeed69bc6
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-10-16 20:25:51 +0600

    Update userslice to get location information automatically

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,8 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getPosition, getAddress } from "../../services/apiGeocoding";
 
 const initialState = {
+    status: "idle", // loading, success, error
     name: "",
+    address: "",
+    position: {},
+    error: null,
 }
+
+async function fetchLocation() {
+    // 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+        latitude: positionObj.coords.latitude,
+        longitude: positionObj.coords.longitude,
+    };
+
+    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+    // 3) Then we return an object with the data that we are interested in
+    return { position, address };
+}
+
+export const fetchAddress = createAsyncThunk("user/fetchAddress", fetchLocation);
+
 const userSlice = createSlice({
     name: "user",
     initialState,
@@ -11,6 +35,19 @@ const userSlice = createSlice({
             state.name = action.payload;
         },
     },
+    extraReducers: (builder) => {
+        builder.addCase(fetchAddress.pending, (state) => {
+            state.status = "loading";
+        }).addCase(fetchAddress.fulfilled, (state, action) => {
+            state.status = "idle";
+            state.position = action.payload.position;
+            state.address = action.payload.address;
+            state.error = null;
+        }).addCase(fetchAddress.rejected, (state, action) => {
+            state.status = "error";
+            state.error = action.error.message;
+        });
+    }
 });
 
 export const { updateUsername } = userSlice.actions;
--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -7,3 +7,9 @@ export async function getAddress({ latitude, longitude }) {
   const data = await res.json();
   return data;
 }
+
+export function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
